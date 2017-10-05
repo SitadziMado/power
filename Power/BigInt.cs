bd9c000f65d62f9d2adadcb776953a3299100c98
LocalUser <LocalUser@K3-304-11>
2017-10-05 13:53:33 +0500

    Сделана функция возведения в степень.

---
+++ Power/BigInt.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Power
+{
+    class BigInt
+    {
+        BigInt(long value)
+        {
+            if (value < 0)
+                sign = true;
+            else
+                sign = false;
+
+            Add(value);
+        }
+
+        void Add(long rhs, int index = 0)
+        {
+            ExpandIfNecessary(index);
+
+            digits[index] += rhs;
+
+            while (digits[index] > Radix)
+            {
+                var temp = digits[index] - Radix;
+                ExpandIfNecessary(index + 1);
+                digits[index + 1] += temp;
+                digits[index] -= Radix;
+                ++index;
+            }
+        }
+
+        void ExpandIfNecessary(int index)
+        {
+            if (digits.Count < index + 1)
+                digits.Add(0);
+        }
+
+        public static BigInt operator+(BigInt lhs, long rhs)
+        {
+            throw new NotImplementedException();
+        }
+
+        private const long Radix = 1000000;
+
+        private bool sign;
+        private List<long> digits = new List<long>();
+    }
+}
--- Power/Program.cs
+++ Power/Program.cs
@@ -10,6 +10,82 @@ namespace Power
     {
         static void Main(string[] args)
         {
+            double number = InputDouble("Введите вещественное число: ");
+            long n = InputNatural("Введите натуральное число: ");
+
+            var result = Pow(number, n);
+            Console.WriteLine("Результат                   : {0}.", result);
+            Console.WriteLine("Результат встроенной функции: {0}.", Math.Pow(number, n));
+        }
+
+        private static double InputDouble(string prompt)
+        {
+            Console.WriteLine(prompt);
+
+            double input;
+            do
+            {
+                try
+                {
+                    input = double.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Требуется вещественное число.");
+                }
+                catch (OverflowException e)
+                {
+                    Console.WriteLine("Попробуйте ввести число поменьше.");
+                }
+            } while (true);
+
+            return input;
+        }
+
+        private static long InputNatural(string prompt)
+        {
+            Console.WriteLine(prompt);
+
+            long input;
+            do
+            {
+                try
+                {
+                    input = long.Parse(Console.ReadLine());
+                    if (input > 0)
+                        break;
+                    else
+                        Console.WriteLine("Требуется натуральное число.");
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Требуется натуральное число.");
+                }
+                catch (OverflowException e)
+                {
+                    Console.WriteLine("Попробуйте ввести число поменьше.");
+                }
+            } while (true);
+
+            return input;
+        }
+
+        private static double Pow(double number, long exp)
+        {
+            double result = 1.0;
+            double accumulate = number;
+
+            while (exp > 0)
+            {
+                if ((exp & 1) == 1)
+                    result *= accumulate;
+
+                accumulate *= accumulate;
+                exp >>= 1;
+            }
+
+            return result;
         }
     }
 }
